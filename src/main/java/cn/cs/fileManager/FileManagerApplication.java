ae24a6081d4b75e0b7cac5916464dfd0f2ca2e14
lyuyu <1969820343@qq.com>
2019-05-23 12:24:02 +0800

    数据库修改之前完好版本

--- src/main/java/cn/cs/fileManager/FileManagerApplication.java
+++ src/main/java/cn/cs/fileManager/FileManagerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 
+
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
